070d53722ce3a9fcc246340b9bf8cf5dc8afa7f1
ismailcanaydin <ismailcanaydin@gmail.com>
2022-10-27 13:43:47 +0300

    .

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import logo from './logo.svg';
+import './App.css';
+import Products from "./pages/Products"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom"
+import Navbar from './Navbar/Navbar';
+import Singin from './pages/Auth/Singin';
+import Singup from './pages/Auth/Singup';
+
+function App() {
+  // let component;
+  // switch (window.location.pathname) {
+  //   case "/":
+  //     component = <Home />;
+  //     break;
+  //   case "/pricing":
+  //     component = <Pricing />;
+  //     break;
+  //   case "/about":
+  //     component = <About />;
+  //     break;
+  // }
+
+  return (
+    <BrowserRouter>
+      <div className="">
+
+
+        <Navbar />
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/singin' element={<Singin />} />
+          <Route path='/singup' element={<Singup />} />
+        </Routes>
+
+
+        {/* {component} */}
+      </div>
+    </BrowserRouter>
+
+  );
+}
+
+
+
+
+export default App;
---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,45 @@
+import styles from "./styles.module.css"
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
+function Navbar() {
+    return (
+        <div>
+            <nav className={styles.nav}>
+                <div className={styles.left}>
+                    <div className='logo'>
+                        <Link to="/">eCommerce</Link>
+                    </div>
+                    <ul className={styles.menu}>
+                        <li>
+                            <Link to="/products">Products</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className={styles.right}>
+                    <Link to="/singin">
+                        <Button colorScheme='purple'>Login</Button>
+                    </Link>
+                    <Link to="/singup">
+                        <Button colorScheme='purple'>Register</Button>
+                    </Link>
+                </div>
+                {/* <Link to="/">Site Name</Link>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/pricing">Pricing</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                </ul> */}
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
