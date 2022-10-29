5b89733ebd6b6109f5833dc85d2ba101ef74e13b
ismailcanaydin <ismailcanaydin@gmail.com>
2022-10-29 15:51:26 +0300

    Card added

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,28 +1,30 @@
 import React from 'react'
-import { Box, Image, Button } from "@chakra-ui/react"
+import { Box, Image, Button, GridItem } from "@chakra-ui/react"
 import { Link } from 'react-router-dom'
+import moment from 'moment'
 
-function Card() {
-  return (
-    <Box borderRadius="1px" borderWidth="lg" overflow="hidden" p="3">
-        <Link to="#/">
-            <Image src='https://picsum.photos/id/1/400/200' alt='product' />
-            <Box p="6">
-                <Box d="flex" alignItems="baseLine">
-                    27/10/2022
-                </Box>
-                <Box mt="1" fontWeight="semibold" as="h4">
-                    Macbook Pro
-                </Box>
-                <Box>
-                    100 TL
-                </Box>
-            </Box>
-        </Link>
 
-        <Button colorScheme="purple">Add to basket</Button>
-    </Box>
-  )
+function Card({ item }) {
+    return (
+        <Box borderRadius="1px" borderWidth="lg" overflow="hidden" p="3">
+            <Link to="#/">
+                <Image src='https://picsum.photos/id/1/400/200' alt='product' />
+                <Box p="6">
+                    <Box d="flex" alignItems="baseLine">
+                        {moment(item.createdAt).format("DD/MM/YYYY")}
+                    </Box>
+                    <Box mt="1" fontWeight="semibold" as="h4">
+                        {item.title}
+                    </Box>
+                    <Box>
+                        {item.price} TL
+                    </Box>
+                </Box>
+            </Link>
+
+            <Button colorScheme="purple">Add to basket</Button>
+        </Box>
+    )
 }
 
 export default Card
