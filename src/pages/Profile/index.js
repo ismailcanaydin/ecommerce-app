c7d92339342e2f34032087a8454f3f59deb075ce
ismailcanaydin <ismailcanaydin@gmail.com>
2022-11-14 23:47:07 +0300

    created

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,16 @@
+import { Text } from '@chakra-ui/react';
+import React from 'react'
+import { useAuth } from '../../contexts/AuthContext'
+
+function Profile() {
+  const { user } = useAuth();
+
+  return (
+    <div>
+      <Text fontSize={22}>Profile</Text>
+      <code>{JSON.stringify(user)}</code>
+    </div>
+  )
+}
+
+export default Profile
