ac29e28cb6ac222245e95bcd3a5ff3b1d0256e97
ismailcanaydin <ismailcanaydin@gmail.com>
2022-11-21 22:46:45 +0300

    order page listed

--- src/pages/Admin/Orders/index.js
+++ src/pages/Admin/Orders/index.js
@@ -1,10 +1,57 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { useQuery } from '@tanstack/react-query'
+import { fetchOrders } from '../../../api'
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+  Text,
+} from '@chakra-ui/react'
 
 function Orders() {
-  return (
-    <div>Orders
+  const { isLoading, isError, data, error } = useQuery(['admin:orders'], fetchOrders);
 
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (isError) {
+    return <div>Error {error.message}</div>
+  }
+
+  console.log(data);
+
+  return (
+    <div>
+      <Text fontSize='2xl' p={5}>
+        Orders
+      </Text>
+
+      <Table variant={'simple'}>
+        <TableCaption></TableCaption>
+        <Thead>
+          <Tr>
+            <Th>User</Th>
+            <Th>Address</Th>
+            <Th>Items</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {data.map((item) => (
+            <Tr key={item._id}>
+              <Td>{item.user.email}</Td>
+              <Td>{item.adress}</Td>
+              <Td>{item.items.length}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
     </div>
   )
 }
